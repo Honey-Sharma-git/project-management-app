acf99867401dee01e63682ab9babf260d8f6e750
Honey-Sharma-git <honeysharmaemail@gmail.com>
2025-04-03 16:04:46 +0530

    Add login api

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,73 @@
-import { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { alert } from "../../utils/common";
+
 export const Login = () => {
-  const [isPassShown, setIsPassShown] = useState(false);
   const navigate = useNavigate();
+  const [isPassShown, setIsPassShown] = useState(false);
+  const [token, setToken] = useState("");
+  const [userId, setUserId] = useState("");
+  const [user, setUser] = useState({
+    email: "honey@gmail.com",
+    password: "password",
+  });
+
   function togglePassShown(e) {
     e.preventDefault();
     setIsPassShown((prev) => {
       return !prev;
     });
   }
-  function login(e) {
-    e.preventDefault();
-    navigate("/dashboard");
+
+  function handleChange(e) {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
   }
+
+  async function login(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://192.168.0.105:8080/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const data = await response.json();
+
+      //On success of API:
+      if (response.status === 200) {
+        //Login successful alert:
+        alert(data?.message);
+
+        //Setting token and userId to localStorage:
+        console.log(data.token);
+        setToken(data.token);
+        setUserId(data.userId);
+      } else {
+        //Error message in alert:
+        alert(data?.message);
+      }
+    } catch (error) {
+      console.log("Error putting data while signing up", error);
+    }
+  }
+  //If token is present move to dashboard
+  useEffect(() => {
+    if (token && userId) {
+      localStorage.setItem("token", token);
+      localStorage.setItem("userId", userId);
+    }
+    if (localStorage.getItem("token")) {
+      navigate("/dashboard");
+    }
+  }, [token]);
+
   return (
     <main className="flex flex-row justify-center items-center min-h-screen p-5 sm:pl-5 md:pl-20 bg-[var(--color-dark-navy)]">
       <article className="w-full rounded-xl flex flex-row sm:gap-5 md:gap-20 drop-shadow-2xl min-h-[90vh] text-[var(--text-color-gray)]">
@@ -39,10 +91,12 @@ export const Login = () => {
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email:</label>
               <input
+                onChange={handleChange}
                 className="p-2 bg-[var(--input-bg-color)] rounded-sm"
                 type="text"
                 name="email"
                 id="email"
+                value={user.email}
                 placeholder="myemail@example.com"
               />
             </div>
@@ -50,10 +104,12 @@ export const Login = () => {
               <label htmlFor="password">Password:</label>
               <div className="relative">
                 <input
+                  onChange={handleChange}
                   className="p-2 pr-10 bg-[var(--input-bg-color)] rounded-sm w-full"
                   type={isPassShown ? "text" : "password"}
                   name="password"
                   id="password"
+                  value={user.password}
                   placeholder="**************"
                 />
                 <button
