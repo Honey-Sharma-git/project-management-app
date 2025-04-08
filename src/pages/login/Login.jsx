69d0fc22d7e1165501b2a2862e6b2128d5771364
Honey-Sharma-git <honeysharmaemail@gmail.com>
2025-04-08 11:32:20 +0530

    Add login page validation and UI changes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -21,14 +21,42 @@ export const Login = () => {
     password: justSignedUpUser.password,
   });
 
+  const [validation, setValidation] = useState({
+    isEmailValid: false,
+    isPasswordValid: false,
+  });
+
   function togglePassShown(e) {
     e.preventDefault();
     setIsPassShown((prev) => {
       return !prev;
     });
   }
+  function formValidation(e) {
+    if (e.target.name === "email") {
+      //Email validation:
+      const emailVal = e.target.value;
+      const isValid =
+        emailVal.includes("@") &&
+        emailVal.includes(".") &&
+        !emailVal.includes(" ");
+      //Setting state for UI:
+      setValidation((prev) => {
+        return { ...prev, isEmailValid: isValid };
+      });
+    } else if (e.target.name === "password") {
+      //Password validation:
+      const passVal = e.target.value;
+      const isValid = !passVal.includes(" ") && passVal.length > 0;
+      //Setting state for UI:
+      setValidation((prev) => {
+        return { ...prev, isPasswordValid: isValid };
+      });
+    }
+  }
 
   function handleChange(e) {
+    formValidation(e);
     setUser((prev) => {
       return { ...prev, [e.target.name]: e.target.value };
     });
@@ -39,31 +67,33 @@ export const Login = () => {
     //Show loading:
     setIsLoading(true);
     try {
-      const response = await fetch("http://192.168.0.105:8080/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-      const data = await response.json();
+      if (validation.isEmailValid && validation.isPasswordValid) {
+        const response = await fetch("http://192.168.0.105:8080/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        });
+        const data = await response.json();
 
-      //On success of API:
-      if (response.status === 200) {
-        //Login successful Alert:
-        Alert(data?.message, "success", msgTime.VERY_SHORT);
+        //On success of API:
+        if (response.status === 200) {
+          //Login successful Alert:
+          Alert(data?.message, "success", msgTime.VERY_SHORT);
 
-        //Setting token and userId to localStorage:
-        setToken(data.token);
-        setUserId(data.userId);
-        setUserNameEmail({ name: data.name, email: data.email });
-        //Remove loading:
-        setIsLoading(false);
-      } else {
-        //Error message in Alert:
-        Alert(data?.message, "warning", msgTime.LONG);
-        //Remove loading:
-        setIsLoading(false);
+          //Setting token and userId to localStorage:
+          setToken(data.token);
+          setUserId(data.userId);
+          setUserNameEmail({ name: data.name, email: data.email });
+          //Remove loading:
+          setIsLoading(false);
+        } else {
+          //Error message in Alert:
+          Alert(data?.message, "warning", msgTime.LONG);
+          //Remove loading:
+          setIsLoading(false);
+        }
       }
     } catch (error) {
       console.log("Error putting data while signing up", error);
@@ -108,20 +138,33 @@ export const Login = () => {
               <label htmlFor="email">Email:</label>
               <input
                 onChange={handleChange}
-                className="p-2 bg-[var(--input-bg-color)] rounded-sm"
+                className={`p-2 bg-[var(--input-bg-color)] rounded-sm ${
+                  validation.isEmailValid
+                    ? "focus:outline-green-300 focus:outline-2"
+                    : "focus:outline-red-400 focus:outline-2"
+                }`}
                 type="text"
                 name="email"
                 id="email"
                 value={user.email}
                 placeholder="myemail@example.com"
               />
+              {user.email && !validation.isEmailValid && (
+                <p role="alert" className="text-red-500 text-sm">
+                  Enter a valid email (Include '@', '.' without space)
+                </p>
+              )}
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="password">Password:</label>
               <div className="relative">
                 <input
                   onChange={handleChange}
-                  className="p-2 pr-10 bg-[var(--input-bg-color)] rounded-sm w-full"
+                  className={`p-2 pr-10 bg-[var(--input-bg-color)] rounded-sm w-full ${
+                    validation.isPasswordValid
+                      ? "focus:outline-green-300 focus:outline-2"
+                      : "focus:outline-red-400 focus:outline-2"
+                  }`}
                   type={isPassShown ? "text" : "password"}
                   name="password"
                   id="password"
@@ -138,6 +181,11 @@ export const Login = () => {
                   {isPassShown ? <FaEyeSlash /> : <FaEye />}
                 </button>
               </div>
+              {user.password && !validation.isPasswordValid && (
+                <p role="alert" className="text-red-500 text-sm">
+                  Enter a valid email (Include '@', '.' without space)
+                </p>
+              )}
             </div>
             <div className="flex flex-row gap-3 px-1">
               <span>
@@ -147,8 +195,11 @@ export const Login = () => {
             </div>
             <div>
               <button
+                disabled={
+                  !(validation.isEmailValid && validation.isPasswordValid)
+                }
                 onClick={login}
-                className={`hover:bg-[var(--btn-hover-color-purple)] cursor-pointer p-2 bg-[var(--btn-color-purple)] rounded-sm w-full shadow-xl shadow-gray-900/50 ${
+                className={`disabled:bg-[var(--btn-color-purple)]/30  disabled:cursor-not-allowed hover:bg-[var(--btn-hover-color-purple)] cursor-pointer p-2 bg-[var(--btn-color-purple)] rounded-sm w-full shadow-xl shadow-gray-900/50 ${
                   isLoading ? "animate-pulse" : null
                 }`}
               >
